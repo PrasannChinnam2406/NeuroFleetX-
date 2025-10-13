523b16277c94daf392a62514599b9bb88b5f3dfd
PrasannChinnam2406 <prasannachinnam24@gmail.com>
2025-10-13 20:09:36 +0530

    first commit

---
+++ Datatype_Programs/Program1.java
@@ -0,0 +1,15 @@
+class DataTypesExample {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 12.5;
+        char c = 'A';
+        boolean d = true;
+        String e = "Java";
+
+        System.out.println("Integer: " + a);
+        System.out.println("Double: " + b);
+        System.out.println("Character: " + c);
+        System.out.println("Boolean: " + d);
+        System.out.println("String: " + e);
+    }
+}
